77d51339bf269b41c5a997157d70395ba245e61e
Paulo Lanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-03-13 18:42:31 -0300

    Create apkts-to-csv.sh

---
+++ apkts-to-csv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -xe
+
+# Print audio packets out to an CSV file
+ffprobe -v quiet -show_packets -select_streams a:0 -read_intervals 110%120 -show_entries packet=pts,pts_time,dts,dts_time,duration,duration_time,size,pos,flags -of csv=p=0 $1 > $2
